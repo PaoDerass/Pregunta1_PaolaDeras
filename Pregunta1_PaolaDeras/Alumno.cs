960ca20570ec2ec25feba8fce1d4278bae699b2d
PaoDerass <paola_deras@unitec.edu>
2024-08-12 20:26:15 -0600

    Interfaz Asignatura

--- Pregunta1_PaolaDeras/Alumno.cs
+++ Pregunta1_PaolaDeras/Alumno.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Pregunta1_PaolaDeras
 {
-    internal class Alumno
+    public class Alumno
     {
+        public string NombreAlumno { get; set;}
+        public string NumeroCuenta { get; set; }
+        public string Email { get; set; }
     }
 }
---
+++ Pregunta1_PaolaDeras/IAsignatura.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Pregunta1_PaolaDeras
+{
+    public interface IAsignatura
+    {
+        double CalcularNotaFinal();
+        double CalcularNotaFinal(int nota1, int nota2, int nota3);
+        string MensajeNotaFinal(double notaFinal);
+        void Imprimir();
+
+    }
+}
